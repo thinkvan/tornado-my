6d15835cc3c1da8d6d021f48f82cacdaa870a2a2
thinkvan <vaan2211025>
2020-04-01 23:07:51 +0800

    init

---
+++ migrations/2_deploy_hasher.js
@@ -0,0 +1,21 @@
+/* global artifacts */
+const path = require('path')
+
+const genContract = require('circomlib/src/mimcsponge_gencontract.js')
+const Artifactor = require('@truffle/artifactor')
+
+module.exports = function(deployer) {
+  return deployer.then( async () =>  {
+    const contractsDir = path.join(__dirname, '..', 'build/contracts')
+    let artifactor = new Artifactor(contractsDir)
+    let contractName = 'Hasher'
+    await artifactor.save({
+      contractName,
+      abi: genContract.abi,
+      unlinked_binary: genContract.createCode('mimcsponge', 220),
+    }).then(async () => {
+      const hasherContract = artifacts.require(contractName)
+      await deployer.deploy(hasherContract)
+    })
+  })
+}
